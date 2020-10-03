a61aa18f29ee0f52e1f3d9d9aeb0b045fb990c60
evatux <evatux@gmail.com>
2020-10-02 22:04:33 -0700

    cc core: adding proto

--- src/cc_core/game.cpp
+++ src/cc_core/game.cpp
@@ -4,8 +4,20 @@
 #include <ctime>
 
 #include "game.hpp"
+#include "proto.hpp"
 
 namespace core {
+
+namespace {
+proto::dir_t to_proto(const pos_t &dir) {
+    assert(dir.is_direction());
+    if (dir == UP) return proto::dir_t::UP;
+    if (dir == RIGHT) return proto::dir_t::RIGHT;
+    if (dir == DOWN) return proto::dir_t::DOWN;
+    if (dir == LEFT) return proto::dir_t::LEFT;
+    return proto::dir_t::UNDEF;
+}
+}
 
 void game_t::generate_players(int num_players) {
     enum { start_snake_length = 3 };
@@ -30,7 +42,9 @@ void game_t::generate_players(int num_players) {
     }
 }
 
-void game_t::generate_loots(int num_loots) {
+proto::message_t game_t::generate_loots(int num_loots) {
+    proto::message_t message;
+
     std::srand(std::time(nullptr));
 
     for (int i = 0; i < num_loots; ++i) {
@@ -38,12 +52,43 @@ void game_t::generate_loots(int num_loots) {
             int rand_val = std::rand() % (size_.x * size_.y);
             const pos_t pos{rand_val % size_.x, rand_val / size_.x};
             if (is_empty_cell(pos)) {
-                loot_t loot{loot_t::type_t::FOOD, pos};
+                auto type = loot_t::type_t::FOOD;
+                loot_t loot{type, pos};
+                message.emplace_back(proto::loot_t{(int)type, pos.x, pos.y});
                 loots_.emplace_back(loot);
                 break;
             }
         }
     }
+
+    return message;
+}
+
+proto::message_t game_t::state_message() const {
+    proto::message_t message;
+    message.emplace_back(proto::screen_t{size_.x, size_.y});
+
+    for (int id = 0; id < (int)players_.size(); ++id) {
+        const auto &snake = players_[id].snake;
+        auto it = snake.begin();
+        // head
+        {
+            proto::dir_t dir = to_proto(snake.head_direction());
+            message.emplace_back(proto::snake_t{
+                    id, proto::snake_part_t::HEAD, it->x, it->y, dir});
+            ++it;
+        }
+        // body
+        for (; it != snake.end(); ++it) {
+            message.emplace_back(proto::snake_t{
+                    id, proto::snake_part_t::BODY, it->x, it->y});
+        }
+    }
+
+    for (const auto &loot: loots_)
+        message.emplace_back(proto::loot_t{(int)loot.type, loot.pos.x, loot.pos.y});
+
+    return message;
 }
 
 bool game_t::is_empty_cell(const pos_t &pos) const {
@@ -63,10 +108,24 @@ void game_t::set_snake_head_direction(int id, const pos_t &head_direction) {
     players_[id].snake.set_head_direction(head_direction);
 }
 
-state_t game_t::step() {
+proto::message_t game_t::remove_player(int id) {
+    proto::message_t message;
+
+    assert(players_[id].is_active == true);
+    players_[id].is_active = false;
+
+    for (const auto &b: players_[id].snake)
+        message.emplace_back(proto::clear_t{b.x, b.y});
+    return message;
+}
+
+proto::message_t game_t::step() {
+    proto::message_t message;
+
     int num_loots = num_loots_on_board;
 
-    for (auto &player: players_) {
+    for (int id = 0; id < num_players(); ++id) {
+        auto &player = players_[id];
         if (!player.is_active) continue;
 
         auto &snake = player.snake;
@@ -79,8 +138,16 @@ state_t game_t::step() {
         auto loot = std::find_if(loots_.begin(), loots_.end(),
                 [&](const loot_t &loot) { return loot.pos == target; });
         if (loot != loots_.end()) {
+            pos_t p = snake.head();
+            message.emplace_back(proto::snake_t{
+                    id, proto::snake_part_t::BODY, p.x, p.y});
+            p += dir;
+            message.emplace_back(proto::snake_t{
+                    id, proto::snake_part_t::HEAD, p.x, p.y, to_proto(dir)});
+
             snake.step(dir, true);
             player.score += 1;
+            num_loots -= 1;
             loots_.erase(loot);
             stop = true;
         }
@@ -89,7 +156,7 @@ state_t game_t::step() {
         // check for collision
         for (const auto &other_player: players_) {
             if (other_player.snake.contains(target)) {
-                player.is_active = false;
+                proto::concatenate(message, remove_player(id));
                 stop = true;
             }
             if (stop) break;
@@ -97,12 +164,23 @@ state_t game_t::step() {
         if (stop) continue;
 
         // oK to step
-        snake.step(dir, false);
+        {
+            pos_t p = snake.head();
+            message.emplace_back(proto::snake_t{
+                    id, proto::snake_part_t::BODY, p.x, p.y});
+            p += dir;
+            message.emplace_back(proto::snake_t{
+                    id, proto::snake_part_t::HEAD, p.x, p.y, to_proto(dir)});
+            p = snake.tail();
+            message.emplace_back(proto::clear_t{p.x, p.y});
+
+            snake.step(dir, false);
+        }
     }
 
-    generate_loots(num_loots);
+    proto::concatenate(message, generate_loots(num_loots));
 
-    return state_t::PLAY;
+    return message;
 }
 
 } // namespace core
--- src/cc_core/game.hpp
+++ src/cc_core/game.hpp
@@ -6,6 +6,7 @@
 #include <list>
 #include <vector>
 
+#include "proto.hpp"
 #include "utypes.hpp"
 
 namespace core {
@@ -32,16 +33,16 @@ struct snake_t {
 
     int length() const { return (int)body_.size(); }
 
-    const pos_t &head() const { assert(!body_.empty()); return *body_.cbegin(); }
-    const pos_t &tail() const { assert(!body_.empty()); return *body_.cend(); }
+    const pos_t &head() const { assert(!body_.empty()); return *body_.begin(); }
+    const pos_t &tail() const { assert(!body_.empty()); return *body_.end(); }
     const pos_t &head_direction() const { return head_direction_; }
 
-    std::list<pos_t>::const_iterator cbegin() const { return body_.cbegin(); }
-    std::list<pos_t>::const_iterator cend() const { return body_.cend(); }
+    std::list<pos_t>::const_iterator begin() const { return body_.cbegin(); }
+    std::list<pos_t>::const_iterator end() const { return body_.cend(); }
 
     bool contains(const pos_t &pos) const {
         return std::any_of(
-                cbegin(), cend(), [&](const pos_t &p) { return p == pos; });
+                begin(), end(), [&](const pos_t &p) { return p == pos; });
     }
 
     // modifiers
@@ -60,10 +61,8 @@ private:
     pos_t head_direction_;
 };
 
-enum class state_t { PLAY, END };
-
 struct loot_t {
-    enum class type_t { FOOD };
+    enum class type_t: int { FOOD };
     type_t type;
     pos_t pos;
 };
@@ -80,10 +79,13 @@ struct game_t {
         generate_loots(num_loots_on_board);
     }
 
+    proto::message_t state_message() const;
+
     const pos_t &size() const { return size_; }
 
     const std::vector<player_t> &players() const { return players_; }
     bool is_player_active(int id) const { return players()[id].is_active; }
+    int num_players() const { return (int)players_.size(); }
 
     const std::list<loot_t> &loots() const { return loots_; }
 
@@ -91,18 +93,19 @@ struct game_t {
 
     // modifiers
     void set_snake_head_direction(int id, const pos_t &direction);
-    state_t step();
+    proto::message_t step();
 
 private:
     pos_t size_;
 
     std::vector<player_t> players_;
-
     std::list<loot_t> loots_;
     const int num_loots_on_board = 1;
 
     void generate_players(int num_players);
-    void generate_loots(int num_loots);
+    proto::message_t generate_loots(int num_loots);
+
+    proto::message_t remove_player(int id);
 };
 
 } // namespace core
