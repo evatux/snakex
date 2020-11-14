aaf3eefdd8aded4688dbe6119d8adb6f3731e724
evatux <evatux@gmail.com>
2020-11-13 21:47:05 -0800

    wip

--- src/cc_websockets_client_ncurses_qt/main.cpp
+++ src/cc_websockets_client_ncurses_qt/main.cpp
@@ -4,21 +4,24 @@
 
 #include "client.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
     QCommandLineParser parser;
-    parser.setApplicationDescription("QtWebSockets example: echoclient");
+    parser.setApplicationDescription("Snakex Qt client (using websockets)");
+    parser.addPositionalArgument("address", "Server address. Default localhost:1234");
+    parser.addOption({{"d", "debug"}, "Debug output [default: off]."});
     parser.addHelpOption();
-
-    QCommandLineOption dbgOption(QStringList() << "d" << "debug",
-            QCoreApplication::translate("main", "Debug output [default: off]."));
-    parser.addOption(dbgOption);
     parser.process(a);
-    bool debug = parser.isSet(dbgOption);
 
-    Client client(QUrl(QStringLiteral("ws://localhost:1234")), debug);
+    const QStringList args = parser.positionalArguments();
+    QString address = args.isEmpty() ? QString("localhost:1234") : args.first();
+    if (!address.contains(':')) address += ":1234";
+    if (!address.startsWith("ws://")) address = QString("ws://") + address;
+
+    const bool debug = parser.isSet("debug");
+
+    Client client(QUrl(address), debug);
     QObject::connect(&client, &Client::closed, &a, &QCoreApplication::quit);
 
     return a.exec();
--- src/cc_websockets_server_qt/main.cpp
+++ src/cc_websockets_server_qt/main.cpp
@@ -8,24 +8,17 @@ int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
     QCommandLineParser parser;
-    parser.setApplicationDescription("QtWebSockets example: echoserver");
+    parser.setApplicationDescription("Snakex Qt server (using websockets)");
     parser.addHelpOption();
 
-    QCommandLineOption dbgOption(QStringList() << "d" << "debug",
-            QCoreApplication::translate("main", "Debug output [default: off]."));
-    parser.addOption(dbgOption);
-    QCommandLineOption portOption(QStringList() << "p" << "port",
-            QCoreApplication::translate("main", "Port for echoserver [default: 1234]."),
-            QCoreApplication::translate("main", "port"), QLatin1String("1234"));
-    parser.addOption(portOption);
-    QCommandLineOption nplayersOption(QStringList() << "n" << "nplayers",
-            QCoreApplication::translate("main", "Number of players [default: 1]."),
-            QCoreApplication::translate("main", "num players"), QLatin1String("1"));
-    parser.addOption(nplayersOption);
+    parser.addOption({{"d", "debug"}, "Debug output [default: off]."});
+    parser.addOption({{"p", "port"}, "Port for server [default: 1234].", "num", "1234"});
+    parser.addOption({{"n", "nplayers"}, "Number of players [default: 1].", "num", "1"});
     parser.process(a);
-    bool debug = parser.isSet(dbgOption);
-    int port = parser.value(portOption).toInt();
-    int nplayers = parser.value(nplayersOption).toInt();
+
+    bool debug = parser.isSet("debug");
+    int port = parser.value("port").toInt();
+    int nplayers = parser.value("nplayers").toInt();
 
     Server *server = new Server(port, nplayers, debug);
     QObject::connect(server, &Server::closed, &a, &QCoreApplication::quit);
